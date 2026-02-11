853d05d1043085a5fb30a5ee3fcf65d2aee299c4
sofiane <sofiane.bouricha.edu@gmail.com>
2026-02-11 11:42:34 +0100

    Security fix

--- src/admin/ProductModal.tsx
+++ src/admin/ProductModal.tsx
@@ -43,7 +43,7 @@ export default function ProductModal({ product, close, reload }: ProductModalPro
       } else {
         query = supabase.from("products").insert(payload)
       }
-      const token = `z25Xc8oXLDy0bRHq0yj2whuneV2m7kt9RlVPt9IVtmEzOdIqI2`;
+      const token = import.meta.env.VITE_TOKEN_WORKER;
       const { error } = await query
       if (error) throw error
       await fetch("https://square-coffee-cache.squarecoffeedem.workers.dev/purge", {
